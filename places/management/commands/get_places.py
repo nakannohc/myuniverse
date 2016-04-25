3dbe8f6408ded7404d7fdced433f32001f616fe7
Chonnakan Rittinon <nakannohc@gmail.com>
2016-04-25 23:31:07 +0700

    new grid

--- places/management/commands/get_places.py
+++ places/management/commands/get_places.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from places.models import Place, Grid
-from places.views import get_detail, radar_search
+from places.views import get_detail, radar_search, text_search
 
 
 class Command(BaseCommand):
@@ -9,6 +9,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         grids = Grid.objects.filter(scanned=False)
         #print 'ddd'
+        
+        '''
         for g in grids:
             #print g.id,
             places = radar_search(str(g.lat), str(g.lng), g.place_type, '3000')
@@ -20,7 +22,7 @@ class Command(BaseCommand):
                                           lng=place_detail['geometry']['location']['lng'])
                 if pp.count() == 0:
                     p = Place(name=place_detail['name'],
-                              place_type='convenience_store',
+                              place_type='text_srisawas',
                               lat=place_detail['geometry']['location']['lat'],
                               lng=place_detail['geometry']['location']['lng'],
                               address=place_detail['formatted_address'],
@@ -32,3 +34,22 @@ class Command(BaseCommand):
             #print '.'
             g.scanned = True
             g.save()
+        '''
+        for g in grids:
+            #print g.id,
+            places = text_search(str(g.lat), str(g.lng), '3000', u'ศรีสวัสดิ์')
+            #print places
+            #print len(places)
+            for place in places:
+                #place_detail = get_detail(place['place_id'])
+                place_detail = place
+                pp = Place.objects.filter(lat=place_detail['geometry']['location']['lat'],
+                                          lng=place_detail['geometry']['location']['lng'])
+                if pp.count() == 0:
+                    p = Place(name=place_detail['name'],
+                              place_type='text_srisawas',
+                              lat=place_detail['geometry']['location']['lat'],
+                              lng=place_detail['geometry']['location']['lng'],
+                              address=place_detail['formatted_address'],
+                              grid=g)
+                    p.save()
