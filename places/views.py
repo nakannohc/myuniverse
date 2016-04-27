4d609cdaa08b9c4262b324f26ed549054ad29bfc
Chonnakan Rittinon <nakannohc@gmail.com>
2016-04-27 16:28:51 +0700

    shopping mall

--- places/views.py
+++ places/views.py
@@ -121,8 +121,8 @@ def list_place(name):
     if name == '7eleven':
         places1 = Place.objects.filter(name__icontains=u'7', place_type='convenience_store')
         places2 = Place.objects.filter(name__icontains=u'เซเ', place_type='convenience_store')
-        places3 = Place.objects.filter(name__icontainds='7', place_type='shopping_mall')
-        places4 = Place.objects.filter(name__icontainds='เซเ', place_type='shopping_mall')
+        places3 = Place.objects.filter(name__icontains='7', place_type='shopping_mall')
+        places4 = Place.objects.filter(name__icontains='เซเ', place_type='shopping_mall')
         places = places1 | places2 | places3 | places4
         dl_link = '/places/exportexcel/?name=' + name
     elif name == 'srisawas':
