7dac6d2963ef68df97c9a332d79f4f63069b1201
Chonnakan Rittinon <nakannohc@gmail.com>
2016-05-14 16:27:03 +0700

    bug

--- places/management/commands/repair_data.py
+++ places/management/commands/repair_data.py
@@ -80,7 +80,7 @@ class Command(BaseCommand):
                         #aprob = SequenceMatcher(None,p['formatted_address'], place.address).ratio()
                         nprob = SequenceMatcher(None, p['name'], place.name).ratio()
 
-                        print '%s --- %s = %f' % (place.name, p['name'], nprob)
+                        #print '%s --- %s = %f' % (place.name, p['name'], nprob)
                         #if aprob > maprob and aprob > 0.7:
                         #    maprob = aprob
                         #   dest = p
