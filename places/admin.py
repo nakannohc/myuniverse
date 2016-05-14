b79e3e2262860120ba900fbe4d5f2ffbdfbbfc2d
Chonnakan Rittinon <nakannohc@gmail.com>
2016-05-14 16:04:44 +0700

    repair

--- places/admin.py
+++ places/admin.py
@@ -4,6 +4,7 @@ from .models import Place, Grid, KeywordSummary
 
 class PlaceAdmin(admin.ModelAdmin):
     exclude = ('grid',)
+    search_fields = ('name',)
 
 admin.site.register(Place, PlaceAdmin)
 admin.site.register(Grid)
