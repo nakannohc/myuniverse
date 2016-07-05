21dbb75972018152bebe654114b0ae90d1c4860f
Chonnakan Rittinon <nakannohc@gmail.com>
2016-07-06 01:35:32 +0700

    get-log

---
+++ places/management/commands/get_log.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+from places.models import Place, Grid, KeywordSummary, keywords
+import xlwt
+
+class Command(BaseCommand):
+    help = 'Get logs'
+
+    def handle(self, *args, **options):
+        wb = xlwt.Workbook()
+        ws = wb.add_sheet('logs', cell_overwrite_ok=True)
+
+        first_col = True
+        col = 0
+        for keyword in keywords:
+            grids = Grid.objects.filter(keyword=keyword).order_by('zone', 'x', 'y')
+            if first_col:
+                row = 0
+                ws.write(row, col, 'Grid')
+                row += 1
+                for grid in grids:
+                    ws.write(row, col, grid.zone)
+                    row += 1
+                col += 1
+                first_col = False
+
+            row = 0
+            ws.write(row, col, keyword)
+            row = 1
+            for grid in grids:
+                ws.write(row, col, grid.count_place)
+                row += 1
+            col += 1
+            wb.save('task_log.xls')
