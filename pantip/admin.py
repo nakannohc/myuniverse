38ae09de9f0c5f86778c7ff2ea1d255aa29c9488
Chonnakan Rittinon <chonnakan@Chonnakans-MacBook-Pro.local>
2017-02-19 16:42:40 +0700

    add filter adminpage

--- pantip/admin.py
+++ pantip/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Topic, MarkTopic
 
+
+class AdminMarkTopic(admin.ModelAdmin):
+    list_filter = ['read']
+
 admin.site.register(Topic)
-admin.site.register(MarkTopic)
+admin.site.register(MarkTopic, AdminMarkTopic)
